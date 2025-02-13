b94c0ad4a8893ad1fb2cd822586362a5287503e6
rivopelu <rivopelu12@gmail.com>
2025-02-14 01:57:51 +0700

    FEAT:setup controller

--- src/main/java/com/tokoganteng/app/controllers/impl/AuthControllerImpl.java
+++ src/main/java/com/tokoganteng/app/controllers/impl/AuthControllerImpl.java
@@ -4,11 +4,18 @@ import com.tokoganteng.app.annotations.BaseController;
 import com.tokoganteng.app.annotations.BaseControllerImpl;
 import com.tokoganteng.app.controllers.AuthController;
 import com.tokoganteng.app.dto.response.BaseResponse;
+import com.tokoganteng.app.services.AuthService;
+import com.tokoganteng.app.utils.ResponseHelper;
+import lombok.RequiredArgsConstructor;
 
 @BaseControllerImpl
+@RequiredArgsConstructor
 public class AuthControllerImpl implements AuthController {
+
+    private final AuthService authService;
+
     @Override
     public BaseResponse ping() {
-        return null;
+        return ResponseHelper.createBaseResponse(authService.ping());
     }
 }
--- src/main/java/com/tokoganteng/app/services/impl/AuthServiceImpl.java
+++ src/main/java/com/tokoganteng/app/services/impl/AuthServiceImpl.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Service;
 public class AuthServiceImpl implements AuthService {
     @Override
     public String ping() {
-        return "";
+        return "PONG";
     }
 }
