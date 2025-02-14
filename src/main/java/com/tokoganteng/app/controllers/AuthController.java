bd3ea5ac25cf720baef7efede14ff08c44bc09ce
rivopelu <rivopelu12@gmail.com>
2025-02-14 17:46:30 +0700

    FEAT:add public access annotations in ping

--- src/main/java/com/tokoganteng/app/controllers/AuthController.java
+++ src/main/java/com/tokoganteng/app/controllers/AuthController.java
@@ -1,12 +1,14 @@
 package com.tokoganteng.app.controllers;
 
 import com.tokoganteng.app.annotations.BaseController;
+import com.tokoganteng.app.annotations.PublicAccess;
 import com.tokoganteng.app.dto.response.BaseResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @BaseController("auth")
 public interface AuthController {
 
+    @PublicAccess
     @GetMapping("ping")
     BaseResponse ping();
 
